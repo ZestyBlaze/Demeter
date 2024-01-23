701a2e093b754a3fe4f968d8e21e591efa049772
ZestyBlaze <68081568+ZestyBlaze@users.noreply.github.com>
2024-01-23 15:03:16 +0000

    Add TODO to remember this

--- src/main/java/dev/zestyblaze/betterfauna/mixin/ChickenMixin.java
+++ src/main/java/dev/zestyblaze/betterfauna/mixin/ChickenMixin.java
@@ -11,6 +11,7 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 public class ChickenMixin {
     @Redirect(method = "aiStep", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/animal/Chicken;spawnAtLocation(Lnet/minecraft/world/level/ItemLike;)Lnet/minecraft/world/entity/item/ItemEntity;"))
     private ItemEntity betterFauna$handleEggLaying(Chicken instance, ItemLike itemLike) {
+        // TODO: Handle the new proper egg laying
         return null;
     }
 }
