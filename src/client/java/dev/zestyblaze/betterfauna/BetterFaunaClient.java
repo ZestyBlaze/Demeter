acde9d97c08681688fcbf43b5b9980cb480ab508
ZestyBlaze <68081568+ZestyBlaze@users.noreply.github.com>
2024-01-23 15:02:15 +0000

    Remove Client Module

--- src/client/java/dev/zestyblaze/betterfauna/BetterFaunaClient.java
+++
@@ -1,9 +0,0 @@
-package dev.zestyblaze.betterfauna;
-
-import net.fabricmc.api.ClientModInitializer;
-
-public class BetterFaunaClient implements ClientModInitializer {
-	@Override
-	public void onInitializeClient() {
-	}
-}
