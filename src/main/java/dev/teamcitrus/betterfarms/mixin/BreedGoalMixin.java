365543bc7fb2178d6c263a1f4af240b414c12145
ZestyBlaze <68081568+ZestyBlaze@users.noreply.github.com>
2024-01-24 23:22:50 +0000

    Set up Breeding

---
+++ src/main/java/dev/teamcitrus/betterfarms/mixin/BreedGoalMixin.java
@@ -0,0 +1,8 @@
+package dev.teamcitrus.betterfarms.mixin;
+
+import net.minecraft.world.entity.ai.goal.BreedGoal;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(BreedGoal.class)
+public class BreedGoalMixin {
+}
