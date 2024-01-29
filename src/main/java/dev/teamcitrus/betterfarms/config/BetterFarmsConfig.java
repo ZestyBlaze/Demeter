6313668600758be812b2afefdbc80147c686e3a1
ZestyBlaze <68081568+ZestyBlaze@users.noreply.github.com>
2024-01-29 01:57:00 +0000

    Update what is needed for the config

--- src/main/java/dev/teamcitrus/betterfarms/config/BetterFarmsConfig.java
+++ src/main/java/dev/teamcitrus/betterfarms/config/BetterFarmsConfig.java
@@ -6,11 +6,6 @@ public class BetterFarmsConfig {
     public static final ModConfigSpec GENERAL_SPEC;
 
     public static ModConfigSpec.IntValue animalHappinessMin;
-    public static ModConfigSpec.BooleanValue pigsMultipleChildren;
-    public static ModConfigSpec.IntValue pigsDaysPregnant;
-    public static ModConfigSpec.IntValue pigsMultipleChildrenMax;
-    public static ModConfigSpec.BooleanValue rabbitsMultipleChildren;
-    public static ModConfigSpec.IntValue rabbitsMultipleChildrenMax;
 
     static {
         ModConfigSpec.Builder configBuilder = new ModConfigSpec.Builder();
@@ -22,20 +17,6 @@ public class BetterFarmsConfig {
         builder.push("All Animals");
         animalHappinessMin = builder.comment("The minimum value of happiness required for an animal to be considered 'happy'")
                 .defineInRange("animalHappinessMin", 65, 0, 100);
-        builder.pop();
-        builder.push("Pigs");
-        pigsMultipleChildren = builder.comment("Can Pigs give birth to multiple piglets?")
-                .define("pigsMultipleChildren", true);
-        pigsDaysPregnant = builder.comment("The number of days in Minecraft that a Pig will be pregnant before giving birth")
-                .defineInRange("pigsDaysPregnant", 4, 1, 99);
-        pigsMultipleChildrenMax = builder.comment("The maximum number of offspring a Pig can have in a litter")
-                .defineInRange("pigsMultipleChildrenMax", 7, 1, 10);
-        builder.pop();
-        builder.push("Rabbits");
-        rabbitsMultipleChildren = builder.comment("Can Rabbits give birth to multiple piglets?")
-                .define("rabbitsMultipleChildren", true);
-        rabbitsMultipleChildrenMax = builder.comment("The maximum number of offspring a Rabbit can have in a litter")
-                .defineInRange("rabbitsMultipleChildrenMax", 6, 1, 10);
         builder.pop();
     }
 }
