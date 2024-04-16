48917ab8f396cefda40928ac70de607afe429733
ZestyBlaze <68081568+ZestyBlaze@users.noreply.github.com>
2024-04-17 00:22:55 +0100

    New Classes ready for new features

---
+++ src/main/java/dev/teamcitrus/betterfarms/item/AnimalTagItem.java
@@ -0,0 +1,9 @@
+package dev.teamcitrus.betterfarms.item;
+
+import net.minecraft.world.item.Item;
+
+public class AnimalTagItem extends Item {
+    public AnimalTagItem() {
+        super(new Properties().stacksTo(16));
+    }
+}
---
+++ src/main/java/dev/teamcitrus/betterfarms/item/BrushItem.java
@@ -0,0 +1,9 @@
+package dev.teamcitrus.betterfarms.item;
+
+import net.minecraft.world.item.Item;
+
+public class BrushItem extends Item {
+    public BrushItem() {
+        super(new Properties().stacksTo(1).durability(156));
+    }
+}
