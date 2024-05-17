6b99e5de411637c371beb744391b3f5d53257bbb
ZestyBlaze <68081568+ZestyBlaze@users.noreply.github.com>
2024-05-17 20:55:24 +0100

    Javadocs to MilkAttachment

--- src/main/java/dev/teamcitrus/betterfarms/attachment/MilkAttachment.java
+++ src/main/java/dev/teamcitrus/betterfarms/attachment/MilkAttachment.java
@@ -3,6 +3,9 @@ package dev.teamcitrus.betterfarms.attachment;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 
+/**
+ * A smaller attachment that attaches itself onto all mobs that can be milked to track if it's been milked each day
+ */
 public class MilkAttachment {
     public static final Codec<MilkAttachment> CODEC = RecordCodecBuilder.create(instance -> instance.group(
             Codec.BOOL.fieldOf("hasBeenMilked").forGetter(o -> o.hasBeenMilked)
