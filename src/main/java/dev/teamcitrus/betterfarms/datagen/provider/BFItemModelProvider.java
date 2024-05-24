4337eb1f524d2bc568f8222f872090d0972eedfa
ZestyBlaze <68081568+ZestyBlaze@users.noreply.github.com>
2024-05-24 13:50:51 +0100

    Update to CitrusItemModelProvider

--- src/main/java/dev/teamcitrus/betterfarms/datagen/provider/BFItemModelProvider.java
+++ src/main/java/dev/teamcitrus/betterfarms/datagen/provider/BFItemModelProvider.java
@@ -2,6 +2,7 @@ package dev.teamcitrus.betterfarms.datagen.provider;
 
 import dev.teamcitrus.betterfarms.BetterFarms;
 import dev.teamcitrus.betterfarms.registry.BlockRegistry;
+import dev.teamcitrus.citruslib.datagen.CitrusItemModelProvider;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.data.PackOutput;
 import net.minecraft.resources.ResourceLocation;
@@ -9,7 +10,7 @@ import net.minecraft.world.level.block.Block;
 import net.neoforged.neoforge.client.model.generators.ItemModelProvider;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
 
-public class BFItemModelProvider extends ItemModelProvider {
+public class BFItemModelProvider extends CitrusItemModelProvider {
     public BFItemModelProvider(PackOutput output, ExistingFileHelper existingFileHelper) {
         super(output, BetterFarms.MODID, existingFileHelper);
     }
@@ -31,19 +32,8 @@ public class BFItemModelProvider extends ItemModelProvider {
         toBlock(BlockRegistry.MAPLE_LEAVES.get());
         basicItem(BlockRegistry.MAPLE_DOOR.get().asItem());
         toBlockModel(BlockRegistry.MAPLE_TRAPDOOR.get(), "maple_trapdoor_bottom");
-
          */
-    }
 
-    private void toBlock(Block b) {
-        toBlockModel(b, BuiltInRegistries.BLOCK.getKey(b).getPath());
-    }
-
-    private void toBlockModel(Block b, String model) {
-        toBlockModel(b, BetterFarms.id("block/" + model));
-    }
-
-    private void toBlockModel(Block b, ResourceLocation model) {
-        withExistingParent(BuiltInRegistries.BLOCK.getKey(b).getPath(), model);
+        toBlock(BlockRegistry.MAPLE_LEAVES.get());
     }
 }
