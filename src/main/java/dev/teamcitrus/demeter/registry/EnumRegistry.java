cb5045ab5539ca437f7820940e2e973b23a7c9aa
ZestyBlaze <68081568+ZestyBlaze@users.noreply.github.com>
2024-08-21 12:50:29 +0100

    Correctly add boat and chest boat

--- src/main/java/dev/teamcitrus/demeter/registry/EnumRegistry.java
+++ src/main/java/dev/teamcitrus/demeter/registry/EnumRegistry.java
@@ -8,8 +8,8 @@ import java.util.function.Supplier;
 
 public class EnumRegistry {
     public static final EnumProxy<Boat.Type> MAPLE_BOAT_TYPE = new EnumProxy<>(
-            Boat.Type.class, WoodSetRegistry.MAPLE.getPlanks(), "demeter:maple", supplier(Items.AIR),
-            supplier(Items.AIR), supplier(Items.STICK), false
+            Boat.Type.class, WoodSetRegistry.MAPLE.getPlanks(), "demeter:maple", ItemRegistry.MAPLE_BOAT,
+            ItemRegistry.MAPLE_CHEST_BOAT, supplier(Items.STICK), false
     );
 
     public static <T> Supplier<T> supplier(T object) {
