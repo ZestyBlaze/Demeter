61c0c5832ecbb71d991b44f394cd293fd697c3c1
ZestyBlaze <68081568+ZestyBlaze@users.noreply.github.com>
2024-09-13 16:15:43 +0100

    Update registry name of Irrigation

--- src/main/java/dev/teamcitrus/demeter/registry/ComponentRegistry.java
+++ src/main/java/dev/teamcitrus/demeter/registry/ComponentRegistry.java
@@ -1,8 +1,8 @@
 package dev.teamcitrus.demeter.registry;
 
 import dev.teamcitrus.demeter.Demeter;
-import dev.teamcitrus.demeter.component.QualityDataComponent;
 import dev.teamcitrus.demeter.component.IrrigationLevelComponent;
+import dev.teamcitrus.demeter.component.QualityDataComponent;
 import net.minecraft.core.component.DataComponentType;
 import net.neoforged.neoforge.fluids.SimpleFluidContent;
 import net.neoforged.neoforge.registries.DeferredHolder;
@@ -13,8 +13,8 @@ public class ComponentRegistry {
 
     public static final DeferredHolder<DataComponentType<?>, DataComponentType<QualityDataComponent>> QUALITY =
             COMPONENTS.registerComponentType("quality", builder -> builder.persistent(QualityDataComponent.CODEC).networkSynchronized(QualityDataComponent.STREAM_CODEC));
-    public static final DeferredHolder<DataComponentType<?>, DataComponentType<IrrigationLevelComponent>> WATERING_CAN_LEVEL =
-            COMPONENTS.registerComponentType("watering_can_level", builder -> builder.persistent(IrrigationLevelComponent.CODEC).networkSynchronized(IrrigationLevelComponent.STREAM_CODEC));
-    public static final DeferredHolder<DataComponentType<?>, DataComponentType<SimpleFluidContent>> FLUID =
-            COMPONENTS.registerComponentType("fluids", builder -> builder.persistent(SimpleFluidContent.CODEC).networkSynchronized(SimpleFluidContent.STREAM_CODEC));
+    public static final DeferredHolder<DataComponentType<?>, DataComponentType<IrrigationLevelComponent>> IRRIGATION_LEVEL =
+            COMPONENTS.registerComponentType("irrigation_level", builder -> builder.persistent(IrrigationLevelComponent.CODEC).networkSynchronized(IrrigationLevelComponent.STREAM_CODEC));
+    public static final DeferredHolder<DataComponentType<?>, DataComponentType<SimpleFluidContent>> FLUID_HANDLER =
+            COMPONENTS.registerComponentType("fluid_handler", builder -> builder.persistent(SimpleFluidContent.CODEC).networkSynchronized(SimpleFluidContent.STREAM_CODEC));
 }
