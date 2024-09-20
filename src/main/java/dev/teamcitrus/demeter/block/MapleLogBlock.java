d7189ec909c4e538f80fb68e5b349d4938f05308
ZestyBlaze <68081568+ZestyBlaze@users.noreply.github.com>
2024-09-21 00:02:29 +0100

    Remove states from Maple Log

--- src/main/java/dev/teamcitrus/demeter/block/MapleLogBlock.java
+++ src/main/java/dev/teamcitrus/demeter/block/MapleLogBlock.java
@@ -3,22 +3,15 @@ package dev.teamcitrus.demeter.block;
 import dev.teamcitrus.demeter.registry.BlockRegistry;
 import net.minecraft.world.item.AxeItem;
 import net.minecraft.world.item.context.UseOnContext;
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.RotatedPillarBlock;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.state.StateDefinition;
-import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.level.material.MapColor;
 import net.neoforged.neoforge.common.ItemAbility;
 
 public class MapleLogBlock extends RotatedPillarBlock {
-    public static final BooleanProperty SYRUP = BooleanProperty.create("syrup");
-    public static final BooleanProperty TAPPED = BooleanProperty.create("tapped");
-
     public MapleLogBlock() {
         super(Properties.ofFullCopy(Blocks.OAK_LOG).mapColor(MapColor.COLOR_ORANGE));
-        this.registerDefaultState(this.defaultBlockState().setValue(SYRUP, false).setValue(TAPPED, false));
     }
 
     @Override
@@ -32,11 +25,5 @@ public class MapleLogBlock extends RotatedPillarBlock {
             }
         }
         return super.getToolModifiedState(state, context, itemAbility, simulate);
-    }
-
-    @Override
-    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> pBuilder) {
-        super.createBlockStateDefinition(pBuilder);
-        pBuilder.add(SYRUP, TAPPED);
     }
 }
